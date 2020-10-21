b6d02d95741b6ed40492c9554e69b8edfa7e0392
sachin <sachinkshuklaoo7@gamil.com>
2020-10-22 02:34:56 +0530

    button click simulation working:

--- app/src/main/java/com/sachin/backgrounddemo/MainActivity.java
+++ app/src/main/java/com/sachin/backgrounddemo/MainActivity.java
@@ -7,7 +7,10 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View myView = findViewById(R.id.startButton);
+        myView.performClick();
     }
 
     public void startBGService(View view) {
+//        Intent intent = new Intent(this, MyBackgroundService.class);
+//        intent.setAction("BackgroundProcess");
+        Log.d("CLICKED", "clicked");
+//
+//        // Set the task
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+//
+//        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+//
+//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 500, pendingIntent);
+//
+//        finish();
+
+//        Toast.makeText(this, "Running from Background", Toast.LENGTH_SHORT).show();
+
+        AlarmManager alarmManager=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, MyBackgroundService.class);
-        intent.setAction("BackgroundProcess");
-
-        // Set the task
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-
-        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 500, pendingIntent);
-
-        finish();
+        alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() +  1000, pendingIntent);
     }
 }
--- app/src/main/java/com/sachin/backgrounddemo/MyBackgroundService.java
+++ app/src/main/java/com/sachin/backgrounddemo/MyBackgroundService.java
@@ -11,14 +11,15 @@ import android.widget.Toast;
 public class MyBackgroundService extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Ringtone ringtone = RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
-
-        ringtone.play();
-
+//        Ringtone ringtone = RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+//
+//        ringtone.play();
+//
         Toast.makeText(context, "Running from Background", Toast.LENGTH_SHORT).show();
+//
+//        SystemClock.sleep(3000);
+//
+//        ringtone.stop();
 
-        SystemClock.sleep(3000);
-
-        ringtone.stop();
     }
 }
